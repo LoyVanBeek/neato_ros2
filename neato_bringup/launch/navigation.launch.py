4fbd93f300112d78b91741f18ed230d5f0ae36fb
Loy van Beek <loy.vanbeek@gmail.com>
2020-11-01 14:49:49 +0100

    WIP: Setting up lifecycle nd trnsitions for AMCL + Map server

--- neato_bringup/launch/navigation.launch.py
+++ neato_bringup/launch/navigation.launch.py
@@ -2,11 +2,15 @@ import os
 from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch import LaunchDescription, LaunchIntrospector
+from launch.events import matches_action
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, EmitEvent, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch_ros.actions import Node, LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
 
+from lifecycle_msgs.msg import Transition
 
 def generate_launch_description():
     map_config = Path(
@@ -19,17 +23,94 @@ def generate_launch_description():
         )
     )
 
-    return LaunchDescription([
-        Node(
-              package='nav2_amcl',
-              executable='amcl',
-              name='amcl',
-              output='screen'),
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[map_config]),
-        navigation2,
+    amcl = LifecycleNode(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen')
+
+    map_server = LifecycleNode(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[map_config])
+
+    activate_amcl = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=amcl, goal_state='inactive',
+            entities=[
+                LogInfo(
+                    msg="AMCL reached the 'inactive' state, 'activating'."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(amcl),
+                    transition_id=Transition.TRANSITION_ACTIVATE,
+                )),
+            ],
+        )
+    )
+
+    configure_amcl = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(amcl),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+    configure_map_server = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(map_server),
+            transition_id=Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+
+    configure_map_server = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=map_server, goal_state='unconfigured',
+            entities=[
+                LogInfo(
+                    msg="AMCL reached the 'unconfigured' state, 'configuring'."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(map_server),
+                    transition_id=Transition.TRANSITION_CONFIGURE,
+                )),
+            ],
+        )
+    )
+
+    activate_map_server = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=map_server, goal_state='configured',
+            entities=[
+                LogInfo(
+                    msg="AMCL reached the 'configured' state, 'activating'."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(map_server),
+                    transition_id=Transition.TRANSITION_ACTIVATE,
+                )),
+            ],
+        )
+    )
+
+    ld = LaunchDescription([
+        # configure_amcl,
+        activate_amcl,
+        amcl,
+        activate_map_server,
+        map_server,
+        # navigation2,
+        configure_map_server,
+        configure_amcl,
       ])
+
+    print('Starting introspection of launch description...')
+    print('')
+
+    print(LaunchIntrospector().format_launch_description(ld))
+
+    print('')
+    print('Starting launch of launch description...')
+    print('')
+
+    return ld
