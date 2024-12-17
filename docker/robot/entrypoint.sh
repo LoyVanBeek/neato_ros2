0f838151eb14015a1b5ed5c76a00ee39da123ba6
Loy van Beek <loy.van.beek@aigro.nl>
2024-12-17 21:04:36 +0100

    Use rmw_zenohd  and run the deamon in a TMux session on the robot

--- docker/robot/entrypoint.sh
+++ docker/robot/entrypoint.sh
@@ -7,5 +7,7 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 # Source the overlay workspace, if built
 source /root/ros2_ws/install/setup.bash
 
+tmux new-session -d -s zenohd 'ros2 run rmw_zenoh_cpp rmw_zenohd'
+
 # Execute the command passed into this entrypoint
 exec "$@"
