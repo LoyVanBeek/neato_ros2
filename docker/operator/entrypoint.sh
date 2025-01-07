58b9cc85a2ecf5deb112def77432c6ff782647a1
Loy van Beek <loy.van.beek@aigro.nl>
2025-01-07 21:58:13 +0100

    Install virtual_joystick

--- docker/operator/entrypoint.sh
+++ docker/operator/entrypoint.sh
@@ -3,8 +3,10 @@
 
 # Source ROS 2
 source /opt/ros/${ROS_DISTRO}/setup.bash
+source /root/ros2_ws/install/setup.bash
 
 tmux new-session -d -s zenohd 'ros2 run rmw_zenoh_cpp rmw_zenohd'
+tmux new-session -d -s joystick 'ros2 launch virtual_joystick vjoy.launch.py'
 
 # Execute the command passed into this entrypoint
 exec rviz2 -d /root/neato.rviz
