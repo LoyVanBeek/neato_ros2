4124045484f4a83411d3847b869be513344650ab
Loy van Beek <loy.van.beek@aigro.nl>
2025-01-09 22:14:41 +0100

    Start Rviz in background tmux session and launch neato_operator/teleop from docker-compose

--- docker/operator/entrypoint.sh
+++ docker/operator/entrypoint.sh
@@ -6,7 +6,7 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 source /root/ros2_ws/install/setup.bash
 
 tmux new-session -d -s zenohd 'ros2 run rmw_zenoh_cpp rmw_zenohd'
-tmux new-session -d -s joystick 'ros2 launch virtual_joystick vjoy.launch.py'
+tmux new-session -d -s rviz 'rviz2 -d /root/neato.rviz'
 
 # Execute the command passed into this entrypoint
-exec rviz2 -d /root/neato.rviz
+exec  "$@"
