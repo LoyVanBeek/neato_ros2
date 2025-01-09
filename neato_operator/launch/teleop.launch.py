faa7286ecab6039e507b8ab7fac691cb830f13c3
Loy van Beek <loy.van.beek@aigro.nl>
2025-01-09 22:12:58 +0100

    Split teleop/operator parts of neato_bringup to separate neato_operator package

--- neato_operator/launch/teleop.launch.py
+++ neato_operator/launch/teleop.launch.py
@@ -10,7 +10,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     speedlink_config_path = Path(
-        get_package_share_directory('neato_bringup'), 'config', 'speedlink.yaml')
+        get_package_share_directory('neato_operator'), 'config', 'speedlink.yaml')
 
     return LaunchDescription([
         Node(
@@ -22,6 +22,20 @@ def generate_launch_description():
             package='teleop_twist_joy',
             executable='teleop_node',
             name='joystick_teleop',
+            output='screen',
+            parameters=[speedlink_config_path]),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('virtual_joystick'),
+                    'launch',
+                    'vjoy.launch.py'),
+            )
+        ),
+        Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='virtual_joystick_teleop',
             output='screen',
             parameters=[speedlink_config_path]),
       ])
