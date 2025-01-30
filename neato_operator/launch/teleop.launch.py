3773811cd93ba9aeacecc9e02ca69136609744f0
Loy van Beek <loy.van.beek@aigro.nl>
2025-01-30 21:46:02 +0100

    Use virtual joystick with proper config. Would be nice to use both configs, but using the remapping, also the cmd_vel ends up in the namespace

--- neato_operator/launch/teleop.launch.py
+++ neato_operator/launch/teleop.launch.py
@@ -3,39 +3,56 @@ from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
 
 
 def generate_launch_description():
     speedlink_config_path = Path(
         get_package_share_directory('neato_operator'), 'config', 'speedlink.yaml')
+    virtual_joy_config_path = Path(
+        get_package_share_directory('neato_operator'), 'config', 'virtual.yaml')
 
     return LaunchDescription([
-        Node(
-              package='joy',
-              executable='joy_node',
-              name='joystick_input',
-              output='screen'),
+        IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(
+                            get_package_share_directory('virtual_joystick'),
+                            'launch',
+                            'vjoy.launch.py'),
+                    )
+                ),
         Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='joystick_teleop',
             output='screen',
-            parameters=[speedlink_config_path]),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory('virtual_joystick'),
-                    'launch',
-                    'vjoy.launch.py'),
-            )
-        ),
-        Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='virtual_joystick_teleop',
-            output='screen',
-            parameters=[speedlink_config_path]),
-      ])
+            parameters=[virtual_joy_config_path]),
+
+        # Push this to a separate namespace: https://docs.ros.org/en/foxy/How-To-Guides/Launch-file-different-formats.html
+        # because now both publish to the same /joy topic but the virtual joystick needs a separate config
+        # include another launch file in the virtual_joy_ns namespace
+        # GroupAction(
+        #     actions=[
+        #         # push-ros-namespace to set namespace of included nodes
+        #         PushRosNamespace('virtual_joy_ns'),
+        #         IncludeLaunchDescription(
+        #             PythonLaunchDescriptionSource(
+        #                 os.path.join(
+        #                     get_package_share_directory('virtual_joystick'),
+        #                     'launch',
+        #                     'vjoy.launch.py'),
+        #             )
+        #         ),
+        #         Node(
+        #             package='teleop_twist_joy',
+        #             executable='teleop_node',
+        #             name='virtual_joystick_teleop',
+        #             output='screen',
+        #             parameters=[virtual_joy_config_path]
+        #         ),
+        #     ]
+        # )
+    ])
